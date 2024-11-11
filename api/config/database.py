4dd221008b048c2567fa9b869a676752974f4a39
Sayli Patil <www.saylipatil1603@gmail.com>
2024-11-11 21:30:42 +0530

    add frontend url to deploy

--- api/config/database.py
+++
@@ -1,9 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MONGODB_URI = os.getenv("MONGODB_URI", "your_mongodb_uri")
-client = AsyncIOMotorClient(MONGODB_URI)
-db = client.mydatabase_db
