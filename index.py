e5bd4cd4bea5ea9c5e9cc2c517de74907f2a663b
Sayli Patil <www.saylipatil1603@gmail.com>
2024-11-12 16:37:10 +0530

    updated-code

--- index.py
+++ index.py
@@ -8,7 +8,10 @@ app = FastAPI()
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://workout-tracker-application-y4gq.vercel.app"],
+    allow_origins=[
+        "https://workout-tracker-application-y4gq.vercel.app",
+        # Add your frontend URL on Render if different
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -17,8 +20,5 @@ app.add_middleware(
 # Include your router
 app.include_router(router)
 
-# Remove static files mounting for Vercel deployment
-# Vercel doesn't support local file system in serverless functions
-
-# This is required for Vercel
+# Required for Render
 app = app
