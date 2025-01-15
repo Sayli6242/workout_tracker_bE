1f5ad8b96dd6fd19a2dbb9f7e437fb15f03d5150
sayli6242 <www.saylipatil1603@gmail.com>
2025-01-15 17:16:22 +0530

    .......

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://workoutplanner-theta.vercel.app"],  # Remove trailing slash
+    allow_origins=["*"],  # Remove trailing slash
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
